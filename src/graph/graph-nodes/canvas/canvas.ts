cae5b4187eb959bba6915b7ae90f6f2531593ea9
Roel Westrik <roelwestrik@gmail.com>
2025-08-02 11:54:11 +0200

    canvas elements no longer being watched

--- src/graph/graph-nodes/canvas/canvas.ts
+++ src/graph/graph-nodes/canvas/canvas.ts
@@ -6,7 +6,7 @@ import { assertIsShape, type Shape } from '@/geometry/shape'
 @GraphNodeType('Canvas', 'Canvas')
 export class Canvas extends GraphNode {
   private input
-  private elementStore: { setElements: (e: Shape[]) => void } | null
+  private elementStore: { setElements: (e: Shape[]) => void; redraw: () => void } | null
 
   constructor(id: string, path: string[]) {
     super(id, path)
@@ -22,20 +22,20 @@ export class Canvas extends GraphNode {
   }
 
   public arm(): void {
-    if (this.elementStore !== null) {
-      this.elementStore.setElements([])
-    }
+    this.elementStore?.setElements([])
 
     super.arm()
   }
 
   protected solve(): void {
     this.elementStore?.setElements(this.input.payload.map((v) => assertIsShape(v)))
+    this.elementStore?.redraw()
   }
 
   public onDestroy(): void {
     super.onDestroy()
 
     this.elementStore?.setElements([])
+    this.elementStore?.redraw()
   }
 }
