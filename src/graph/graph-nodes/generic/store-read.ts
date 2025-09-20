ee18e9a20eb62dbc1e444f64486c41adeaf5871c
Roel Westrik <roelwestrik@gmail.com>
2025-09-20 11:42:28 +0200

    cleanup store nodes

--- src/graph/graph-nodes/generic/store-read.ts
+++ src/graph/graph-nodes/generic/store-read.ts
@@ -2,96 +2,54 @@ import type { JsonValue } from '@/graph/core/models/json-value'
 import { GraphNodeType } from '../decorators'
 import { GraphNode } from '@/graph/core/graph-node'
 import type { InputIteratorsAsync } from '@/graph/core/input-iterators-async'
-
-const stores = new Map<string, ReturnType<typeof createStore>>()
-
-function createStore(initialData: JsonValue[]) {
-  const data: JsonValue[] = [...initialData]
-  const update = (values: JsonValue[]) => {
-    data.length = 0
-    for (const value of values) {
-      data.push(value)
-    }
-  }
-  return { data, update }
-}
-
-export function read(name: string) {
-  const store = stores.get(name)
-  if (!store) {
-    throw Error(`Cannot find store ${name}`)
-  }
-
-  return store
-}
-
-export function initialize(name: string, initialData: JsonValue[]) {
-  if (!stores.has(name)) {
-    stores.set(name, createStore(initialData))
-  }
-  const store = stores.get(name)!
-  store?.update(initialData)
-  return store
-}
+import { nanoid } from 'nanoid'
+import { deleteStore, initialize, read, type Store } from './store'
 
 @GraphNodeType('Generic', 'Store', 'Reader')
 export class StoreReader extends GraphNode {
-  private name
   private initial
   private reset
-  private trigger
-  private output
+  private name
+  private values
+
+  public override data: {
+    name: string
+  }
 
   constructor(id: string, path: string[]) {
     super(id, path)
-
-    this.name = this.registerStringInput('Name')
     this.initial = this.registerUnkownInput('Initial')
     this.reset = this.registerBooleanInput('Reset')
-    this.trigger = this.registerUnkownInput('Trigger')
-    this.output = this.registerUnkownOutput('Values')
+    this.registerUnkownInput('Trigger') // ensures dependency
+
+    this.name = this.registerStringOutput('Name')
+    this.values = this.registerUnkownOutput('Values')
+    this.data = { name: nanoid(11) }
   }
 
   protected override async solve(iterators: InputIteratorsAsync): Promise<void> {
-    const [name, reset] = iterators.singletonOnly(this.name, this.reset)
+    const [reset] = iterators.singletonOnly(this.reset)
 
-    let store
+    const name = this.data.name
+    let store: Store
     if (reset) {
-      const initialData = []
-
+      const initialData: JsonValue[] = []
       for await (const value of iterators.createGenerator(this.initial)) {
         initialData.push(value)
       }
-
       store = initialize(name, initialData)
     } else {
       store = read(name)
     }
 
     for (const value of store.data) {
-      this.output.next(value)
+      this.values.next(value)
     }
-  }
-}
 
-@GraphNodeType('Generic', 'Store', 'Writer')
-export class StoreWriter extends GraphNode {
-  private name
-  private input
-
-  constructor(id: string, path: string[]) {
-    super(id, path)
-
-    this.name = this.registerStringInput('Name')
-    this.input = this.registerUnkownInput('Values')
+    this.name.next(this.data.name)
   }
 
-  protected override async solve(iterators: InputIteratorsAsync): Promise<void> {
-    const [name] = iterators.singletonOnly(this.name)
-    const values: JsonValue[] = []
-    for await (const value of iterators.createGenerator(this.input)) {
-      values.push(value)
-    }
-    read(name).update(values)
+  override onDestroy(): void {
+    deleteStore(this.data.name)
   }
 }
---
+++ src/graph/graph-nodes/generic/store-write.ts
@@ -0,0 +1,32 @@
+import { GraphNode } from "@/graph/core/graph-node"
+import type { InputIteratorsAsync } from "@/graph/core/input-iterators-async"
+import type { JsonValue } from "@/graph/core/models/json-value"
+import { GraphNodeType } from "../decorators"
+import { listStores, read } from "./store"
+
+@GraphNodeType('Generic', 'Store', 'Writer')
+export class StoreWriter extends GraphNode {
+  private name
+  private input
+
+  constructor(id: string, path: string[]) {
+    super(id, path)
+    this.name = this.registerStringInput('Name')
+    this.input = this.registerUnkownInput('Values')
+  }
+
+  protected override async solve(iterators: InputIteratorsAsync): Promise<void> {
+    const [name] = iterators.singletonOnly(this.name)
+    const values: JsonValue[] = []
+    for await (const value of iterators.createGenerator(this.input)) {
+      values.push(value)
+    }
+
+    // Auto-initialize if store doesn't exist yet
+    if (!listStores().includes(name)) {
+      throw Error(`Store with name {name} does not exist! Please initialize it first.`)
+    } else {
+      read(name).update(values)
+    }
+  }
+}
